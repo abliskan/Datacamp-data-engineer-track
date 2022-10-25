d402c4599a5238b57b8b94203dbcf9590ed75ae2
RickyS <suhanryricky@yahoo.com>
2022-10-25 22:04:23 +0700

    Update chapter2: Inheritance&Polymorp

--- Object-oriented-programming-in-python/Chapter2/script.py
+++ Object-oriented-programming-in-python/Chapter2/script.py
@@ -63,9 +63,172 @@ print(bd.day)
 # Section6: Understanding inheritance
 # True
 """
-Inheritance represents is-a relationshop
-Running ind = Indexer() will cause an error
-If ind is an Indexer object, then isinstance(ind, Counter) will return True
-Class Indexer is inherited from Counter
+* Inheritance represents is-a relationshop
+* Running ind = Indexer() will cause an error
+* If ind is an Indexer object, then isinstance(ind, Counter) will return True
+* Class Indexer is inherited from Counter
 """
 # False
+"""
+* Every Counter object is an Indexer object
+* If ind Indexer object, then running ind.add_counts(5) will cause an error
+* Inheritance can be used to add some of the parts of one class to another class
+"""
+# Section7: Create a subclass
+"""
+class Employee:
+  MIN_SALARY = 30000    
+
+  def __init__(self, name, salary=MIN_SALARY):
+      self.name = name
+      if salary >= Employee.MIN_SALARY:
+        self.salary = salary
+      else:
+        self.salary = Employee.MIN_SALARY
+        
+  def give_raise(self, amount):
+      self.salary += amount      
+        
+# Define a new class Manager inheriting from Employee
+class Manager(Employee):
+  pass
+
+# Define a Manager object
+mng = Manager("Debbie Lashko", 86500)
+
+# Print mng's name
+print(mng.name)
+###################################################
+class Employee:
+  MIN_SALARY = 30000    
+
+  def __init__(self, name, salary=MIN_SALARY):
+      self.name = name
+      if salary >= Employee.MIN_SALARY:
+        self.salary = salary
+      else:
+        self.salary = Employee.MIN_SALARY
+  def give_raise(self, amount):
+    self.salary += amount      
+        
+# MODIFY Manager class and add a display method
+class Manager(Employee):
+  def display(self):
+    print("Manager", self.name)
+
+mng = Manager("Debbie Lashko", 86500)
+print(mng.name)
+
+# Call mng.display()
+mng.display()
+"""
+# Section8: Customizing functionality via inheritance
+# Section9: Method inheritance
+"""
+class Employee:
+    def __init__(self, name, salary=30000):
+        self.name = name
+        self.salary = salary
+
+    def give_raise(self, amount):
+        self.salary += amount
+
+        
+class Manager(Employee):
+  # Add a constructor 
+    def __init__(self, name, salary=50000, project=None):
+
+        # Call the parent's constructor   
+        Employee.__init__(self, name, salary)
+
+        # Assign project attribute
+        self.project = project  
+
+  
+    def display(self):
+        print("Manager ", self.name)
+###################################################
+class Employee:
+    def __init__(self, name, salary=30000):
+        self.name = name
+        self.salary = salary
+
+    def give_raise(self, amount):
+        self.salary += amount
+
+        
+class Manager(Employee):
+    def display(self):
+        print("Manager ", self.name)
+
+    def __init__(self, name, salary=50000, project=None):
+        Employee.__init__(self, name, salary)
+        self.project = project
+
+    # Add a give_raise method
+    def give_raise(self, amount, bonus = 1.05):
+        new_amount = amount * bonus
+        Employee.give_raise(self, new_amount)
+
+    
+    
+mngr = Manager("Ashta Dunbar", 78500)
+mngr.give_raise(1000)
+print(mngr.salary)
+mngr.give_raise(2000, bonus=1.03)
+print(mngr.salary)
+"""
+# Section10: Inheritance of class attributes
+"""
+# Create a Racer class and set MAX_SPEED to 5
+class Racer(Player):
+    MAX_SPEED = 5
+ 
+# Create a Player and a Racer objects
+p = Player()
+r = Racer()
+
+print("p.MAX_SPEED = ", p.MAX_SPEED)
+print("r.MAX_SPEED = ", r.MAX_SPEED)
+
+print("p.MAX_POSITION = ", p.MAX_POSITION)
+print("r.MAX_POSITION = ", r.MAX_POSITION)
+###################################################
+A: Class attributes can be inherited, and the value of class attributes can be overwritten in the child class
+"""
+# Section11: Customizing a dataFrame
+"""
+# Import pandas as pd
+import pandas as pd
+
+# Define LoggedDF inherited from pd.DataFrame and add the constructor
+class LoggedDF(pd.DataFrame):
+
+    def __init__(self, *args, **kwargs):
+        pd.DataFrame.__init__(self, *args, **kwargs)
+        self.created_at = datetime.today()
+    
+ldf = LoggedDF({"col1": [1,2], "col2": [3,4]})
+print(ldf.values)
+print(ldf.created_at)
+###################################################
+# Import pandas as pd
+import pandas as pd
+
+# Define LoggedDF inherited from pd.DataFrame and add the constructor
+class LoggedDF(pd.DataFrame):
+  
+  def __init__(self, *args, **kwargs):
+    pd.DataFrame.__init__(self, *args, **kwargs)
+    self.created_at = datetime.today()
+    
+  def to_csv(self, *args, **kwargs):
+    # Copy self to a temporary DataFrame
+    temp = self.copy()
+    
+    # Create a new column filled with self.created_at
+    temp["created_at"] = self.created_at
+    
+    # Call pd.DataFrame.to_csv on temp, passing in *args and **kwargs
+    pd.DataFrame.to_csv(temp, *args, **kwargs)
+"""
